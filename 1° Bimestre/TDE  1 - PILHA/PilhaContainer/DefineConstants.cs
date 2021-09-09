9b7b157f1871dd84c8483e57010b5425ec318feb
HenrithanN <61565111+HenrithanN@users.noreply.github.com>
2021-09-09 20:45:04 -0300

    TDE 1 - Pilhas 12-09-2021

---
+++ 1° Bimestre/TDE  1 - PILHA/PilhaContainer/DefineConstants.cs
@@ -0,0 +1,5 @@
+﻿internal static class DefineConstants
+{
+	public const int tamPilha = 3; //quantidade de containers em cada local
+	public const int tamPorto = 4; //quantidade de locais do porto seco
+}
